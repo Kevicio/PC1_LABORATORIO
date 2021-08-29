d521ddabb0f8c32c040731864d30a4beb6d7c48d
Kevin Ruiz <capin.kevin@gmail.com>
2021-08-28 22:28:18 -0500

    Estructura Inicial del Proyecto

---
+++ demomvc/Controllers/ProductoController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using demomvc.Models;
+
+
+namespace demomvc.Controllers
+{
+    public class ProductoController:Controller
+    {
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    
+       
+    }
+
+}
---
+++ demomvc/Models/Producto.cs
@@ -0,0 +1,27 @@
+namespace demomvc.Models
+{
+    public class Producto
+    {
+
+     public string nombre{
+        get;
+        set;
+    }
+    public string categoria{
+        get;
+        set;
+    }
+    public double precio{
+        get;
+        set;
+    }
+    public string descuento{
+        get;
+        set;
+    }
+
+        
+        
+
+    }
+}
